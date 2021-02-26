965def95197c02fab9471514b62c0c4e806add9e
Maga1802 <seenimaga45@gmail.com>
2021-02-26 09:43:09 +0530

    I have done my code

--- src/Org/fb/Task1.java
+++ src/Org/fb/Task1.java
@@ -3,6 +3,7 @@ package Org.fb;
 public class Task1 {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
+		System.out.println("Greens Technology");
 	}
 
 }
---
+++ src/Org/fb/Task3.java
@@ -0,0 +1,9 @@
+package Org.fb;
+
+public class Task3 {
+	public static void main(String[] args) {
+		int a,b=10,c=8;
+		
+	}
+
+}
